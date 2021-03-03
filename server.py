8bb77761f969c1a7917f63b73ece1c463d076dde
dano <mr.quadbit@gmail.com>
2021-03-02 22:59:52 -0600

    Servidor básico con POST e index básico con canvas, todo corre sobre un venv así que instalar requirements.txt

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, Response, jsonify
+import json
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route("/prediction", methods=["POST"])
+def prediction():
+	if request.method == "POST":
+		print(request)
+		response = json.loads(request.data)
+		print(response["number"])
+		return jsonify({"message": randint(0, 9)})
+
+
+if __name__ == "__main__":
+	app.run(debug=True, port=4040)
